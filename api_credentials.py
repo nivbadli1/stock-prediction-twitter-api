d5a6b25a0c1cb944756f3d2c279e93643ac9c276
nivbadli1 <nivbdbd@gmail.com>
2022-08-24 20:51:10 +0300

    f_commit

---
+++ api_credentials.py
@@ -0,0 +1,10 @@
+
+TWITTER_ACCESS_TOKEN = "########-##############"
+TWITTER_ACCESS_TOKEN_SECRET = "##########"
+TWITTER_CONSUMER_KEY = "##########"
+TWITTER_CONSUMER_SECRET = "##########"
+
+ALPACA_ENDPOINT_URL = "https://paper-api.alpaca.markets"
+ALPACA_API_KEY = "PKM1QW32WSSYWD5HVUS9"
+ALPACA_SECRET_KEY = "63flGyq4xYu2qQ5fXwQz4uqn0UbG1uHhCOJdcbhM"
+
